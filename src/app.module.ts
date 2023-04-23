defee47611640d98ed2d54109d08e7fcd5493a4c
marcelobicudo <marcelobicudo.silva@gmail.com>
2023-04-23 06:05:37 -0300

    creating product module

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,7 +1,8 @@
 import { Module } from '@nestjs/common';
 import { UserModule } from './user/user.module';
+import { productModule } from './product/product.module';
 
 @Module({
-  imports: [UserModule],
+  imports: [UserModule, productModule],
 })
 export class AppModule {}
